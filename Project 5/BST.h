46412417e33dc3a8c786e7ae6a05af1d7c8d919c
bnnebedum <148578923+bnnebedum@users.noreply.github.com>
2023-11-26 23:48:58 -0500

    Add files via upload

---
+++ Project 5/BST.h
@@ -0,0 +1,16 @@
+// Bede Nnebedum
+// CSDP 250 - Fall 2023
+// Project 5
+// Header File
+
+#ifndef BST_h
+#define BST_h
+
+struct TreeNode
+{
+    int value;
+    TreeNode *rightNext;
+    TreeNode *leftNext;
+};
+
+#endif
